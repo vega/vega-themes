7b4e683706abf993f0f499e6f608b7c3216f496c
Philipp Kats <casyfill@gmail.com>
2018-05-28 14:51:11 -0400

    formatting

--- src/theme-fiveThirtyEight.ts
+++ src/theme-fiveThirtyEight.ts
@@ -4,108 +4,99 @@ const markColor = "#30a2da";
 const axisColor = "#f0f0f0";
 
 const fiveThirtyEightTheme: Config = {
-  background: '#f0f0f0',
-  symbol: { opacity: 0.5,
-            shape: 'circle',
-            filled: true,
-            size: 40},
   arc: { fill: markColor },
   area: { fill: markColor },
-  line: { stroke: markColor,
-          strokeWidth: 2,
-          strokeCap: 'butt'},
-  path: { stroke: markColor, strokeWidth: 0.5 },
-  rect: { fill: markColor },
-  shape: { stroke: markColor },
-
-  style: {
-    bar: {
-      fill: markColor,
-      binSpacing: 2,
-      stroke: null
-    }
-  },
 
   axisBand: {
     grid: false,
   },
 
+  axisBottom: {
+    domain: false,
+    grid: true,
+    gridColor: "#cbcbcb",
+    gridWidth: 1,
+    tickColor: "#cbcbcb",
+    domainColor: "black",
+    domainWidth: 3,
+    tickSize: 10,
+    labelPadding: 4,
+    labelFontSize: 12,
+    titleFontSize: 14,
+    labelPadding: 4,
+    labelFontSize: 12,
+    titleFontSize: 14,
+    titlePadding: 10
+  },
+
   axisLeft: {
-      
-      gridColor: "#cbcbcb",
-      gridWidth: 1,
-      domainWidth:1,
-      domainColor:"#cbcbcb",
-      ticks: true,
-      tickColor: "#cbcbcb",
-      tickSize: 10,
-      labelPadding:4,
-      labelFontSize: 12,
-      titleFontSize: 14,
-      labelPadding:4,
-      labelFontSize: 12,
-      titleFontSize: 14, 
-      titlePadding: 10
+    domainColor: "#cbcbcb",
+    domainWidth: 1,
+    gridColor: "#cbcbcb",
+    gridWidth: 1,
+    labelPadding: 4,
+    labelFontSize: 12,
+    ticks: true,
+    tickColor: "#cbcbcb",
+    tickSize: 10,
+    titleFontSize: 14,
+    titleFontSize: 14,
+    titlePadding: 10
 
-    },
-    
-    axisBottom: {
-      domain: false,
-      grid: true,
-      gridColor: "#cbcbcb",
-      gridWidth: 1,
-      tickColor: "#cbcbcb",
-      domainColor:"black",
-      domainWidth: 3,
-      tickSize: 10,
-      labelPadding:4,
-      labelFontSize: 12,
-      titleFontSize: 14,
-      labelPadding:4,
-      labelFontSize: 12,
-      titleFontSize: 14, 
-      titlePadding: 10 
-         },
+  },
 
-    axisRight: {
-      
-      gridColor: "#cbcbcb",
-      gridWidth: 1,
-      domainWidth:1,
-      domainColor:"#cbcbcb",
-      ticks: true,
-      tickColor: "#cbcbcb",
-      tickSize: 10,
-      labelPadding:4,
-      labelFontSize: 12,
-      titleFontSize: 14, 
-      titlePadding: 10
-    },
-    
-    axisTop: {
-      domain: false,
-      grid: true,
-      gridColor: "#cbcbcb",
-      gridWidth: 1,
-      tickColor: "#cbcbcb",
-      domainColor:"black",
-      domainWidth: 3,
-      tickSize: 10,
-      labelPadding:4,
-      labelFontSize: 12,
-      titleFontSize: 14, 
-      titlePadding: 10
 
-    },
+
+  axisRight: {
+
+    gridColor: "#cbcbcb",
+    gridWidth: 1,
+    domainWidth: 1,
+    domainColor: "#cbcbcb",
+    ticks: true,
+    tickColor: "#cbcbcb",
+    tickSize: 10,
+    labelPadding: 4,
+    labelFontSize: 12,
+    titleFontSize: 14,
+    titlePadding: 10
+  },
+
+  axisTop: {
+    domain: false,
+    domainColor: "black",
+    domainWidth: 3,
+    labelPadding: 4,
+    labelFontSize: 12,
+    grid: true,
+    gridColor: "#cbcbcb",
+    gridWidth: 1,
+    tickColor: "#cbcbcb",
+    tickSize: 10,
+    titleFontSize: 14,
+    titlePadding: 10
+
+  },
+
+  background: '#f0f0f0',
 
   legend: {
     labelFontSize: 11,
     padding: 1,
     symbolSize: 30,
     symbolType: 'square',
-    titleFontSize: 14, 
+    titleFontSize: 14,
     titlePadding: 10
   },
+
+  line: {
+    stroke: markColor,
+    strokeWidth: 2,
+    strokeCap: 'butt'
+  },
+
+  path: { stroke: markColor, strokeWidth: 0.5 },
+  rect: { fill: markColor },
 
   range: {
     category: [
@@ -116,6 +107,24 @@ const fiveThirtyEightTheme: Config = {
       "#8b8b8b"
     ],
   },
+
+  symbol: {
+    filled: true,
+    opacity: 0.5,
+    shape: 'circle',
+    size: 40
+  },
+
+  shape: { stroke: markColor },
+
+  style: {
+    bar: {
+      fill: markColor,
+      binSpacing: 2,
+      stroke: null
+    }
+  },
+
 };
 
 export default fiveThirtyEightTheme;

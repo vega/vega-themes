043939bd4f15a45e4ac2e89c7608f08689a6c609
palewire <ben.welsh@gmail.com>
2019-02-10 15:39:46 -0800

    Linted latimes theme

--- src/theme-latimes.ts
+++ src/theme-latimes.ts
@@ -13,51 +13,51 @@ const titleFont = 'Benton Gothic Bold, sans';
 const titleFontWeight = 'normal';
 const titleFontSize = 13;
 const colorSchemes = {
-    'category-6': [
-        '#ec8431',
-        '#829eb1',
-        '#c89d29',
-        '#3580b1',
-        '#adc839',
-        '#ab7fb4'
-    ],
-    'fire-7': [
-        '#fbf2c7',
-        '#f9e39c',
-        '#f8d36e',
-        '#f4bb6a',
-        '#e68a4f',
-        '#d15a40',
-        '#ab4232',
-    ],
-    'fireandice-6': [
-        '#e68a4f',
-        '#f4bb6a',
-        '#f9e39c',
-        '#dadfe2',
-        '#a6b7c6',
-        '#849eae'
-    ],
-    'ice-7': [
-        '#edefee',
-        '#dadfe2',
-        '#c4ccd2',
-        '#a6b7c6',
-        '#849eae',
-        '#607785',
-        '#47525d'
-    ]
-}
+  'category-6': [
+    '#ec8431',
+    '#829eb1',
+    '#c89d29',
+    '#3580b1',
+    '#adc839',
+    '#ab7fb4',
+  ],
+  'fire-7': [
+    '#fbf2c7',
+    '#f9e39c',
+    '#f8d36e',
+    '#f4bb6a',
+    '#e68a4f',
+    '#d15a40',
+    '#ab4232',
+  ],
+  'fireandice-6': [
+    '#e68a4f',
+    '#f4bb6a',
+    '#f9e39c',
+    '#dadfe2',
+    '#a6b7c6',
+    '#849eae',
+  ],
+  'ice-7': [
+    '#edefee',
+    '#dadfe2',
+    '#c4ccd2',
+    '#a6b7c6',
+    '#849eae',
+    '#607785',
+    '#47525d',
+  ],
+};
 
 const latimesTheme: Config = {
   background: '#ffffff',
 
   title: {
-      anchor: 'start',
-      font: titleFont,
-      fontColor: '#000000',
-      fontSize: headlineFontSize,
-      fontWeight: headlineFontWeight
+    anchor: 'start',
+    font: titleFont,
+    fontColor: '#000000',
+    fontSize: headlineFontSize,
+    fontWeight: headlineFontWeight,
   },
 
   arc: { fill: markColor },
@@ -69,29 +69,29 @@ const latimesTheme: Config = {
   symbol: { fill: markColor, size: 30 },
 
   axis: {
-      labelFont,
-      labelFontSize,
-      labelFontWeight,
-      titleFont,
-      titleFontSize,
-      titleFontWeight
+    labelFont,
+    labelFontSize,
+    labelFontWeight,
+    titleFont,
+    titleFontSize,
+    titleFontWeight,
   },
 
   axisX: {
-      labelAngle: 0,
-      labelPadding: 4,
-      tickSize: 3
+    labelAngle: 0,
+    labelPadding: 4,
+    tickSize: 3,
   },
 
   axisY: {
-      labelBaseline: 'middle',
-      maxExtent: 45,
-      minExtent: 45,
-      tickSize: 2,
-      titleAlign: 'left',
-      titleAngle: 0,
-      titleX: -45,
-      titleY: -11
+    labelBaseline: 'middle',
+    maxExtent: 45,
+    minExtent: 45,
+    tickSize: 2,
+    titleAlign: 'left',
+    titleAngle: 0,
+    titleX: -45,
+    titleY: -11,
   },
 
   legend: {
@@ -100,7 +100,7 @@ const latimesTheme: Config = {
     symbolType: 'square',
     titleFont,
     titleFontSize,
-    titleFontWeight
+    titleFontWeight,
   },
 
   range: {
@@ -108,9 +108,8 @@ const latimesTheme: Config = {
     diverging: colorSchemes['fireandice-6'],
     heatmap: colorSchemes['fire-7'],
     ordinal: colorSchemes['fire-7'],
-    ramp: colorSchemes['fire-7']
-  }
-
+    ramp: colorSchemes['fire-7'],
+  },
 };
 
 export default latimesTheme;

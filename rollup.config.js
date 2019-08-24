ab22202c00aaa01f7c00695deff558a147163232
Dominik Moritz <domoritz@gmail.com>
2019-08-24 16:34:38 -0700

    Upgrade dev config

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,13 @@
+import json from 'rollup-plugin-json';
+
 export default {
-  input: 'build/index.js',
+  input: 'build/src/index.js',
   output: {
     file: 'build/vega-themes.js',
     format: 'umd',
     sourcemap: true,
     name: 'vegaThemes',
     exports: 'named'
-  }
+  },
+  plugins: [json()]
 };

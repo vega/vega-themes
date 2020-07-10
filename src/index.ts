410bf814dccd5c79ce157a29d24aceab288ea8d2
Dominik Moritz <domoritz@gmail.com>
2020-07-09 17:17:16 -0700

    Revert "refactor: simplify version import"
    This reverts commit 3189379a72a048e6bbafd8d57d85c34218e4f993.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import {version} from '../package.json';
+import pkg from '../package.json';
+const version = pkg.version;
 
 export {default as dark} from './theme-dark';
 export {default as excel} from './theme-excel';

3871344aae13def8723a246bcd8e795cd90db602
Dominik Moritz <domoritz@gmail.com>
2020-09-06 15:41:19 -0700

    chore: modernize build system

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,51 @@
-import json from "@rollup/plugin-json";
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
+import resolve from '@rollup/plugin-node-resolve';
+import ts from '@wessberg/rollup-plugin-ts';
+import bundleSize from 'rollup-plugin-bundle-size';
+import { terser } from 'rollup-plugin-terser';
 
-export default {
-  input: 'build/src/index.js',
-  output: {
-    file: 'build/vega-themes.js',
-    format: 'umd',
-    sourcemap: true,
-    name: 'vegaThemes',
-    exports: 'named'
-  },
-  plugins: [json()]
-};
+const pkg = require('./package.json');
+
+const plugins = [
+  resolve({ extensions: ['.js', '.ts'] }),
+  commonjs(),
+  json(),
+  ts({
+    browserslist: 'defaults and not IE 11'
+  }),
+  bundleSize()
+];
+
+const outputs = [
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'build/vega-themes.module.js',
+      format: 'esm'
+    },
+    plugins,
+    external: [...Object.keys(pkg.peerDependencies)]
+  }, {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: `build/vega-themes.js`,
+        format: 'iife',
+        name: 'vegaThemes',
+        exports: 'named'
+      },
+      {
+        file: `build/vega-themes.min.js`,
+        format: 'iife',
+        name: 'vegaThemes',
+        exports: 'named',
+        plugins: [terser()]
+      }
+    ],
+    plugins,
+    external: ['vega']
+  }
+];
+
+export default outputs;

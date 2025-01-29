bc589e58d6fa539c6210258967d005a981538458
Dominik Moritz <domoritz@gmail.com>
2025-01-29 11:13:42 -0500

    copy some config from embed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,68 +1,79 @@
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsparser from '@typescript-eslint/parser';
-import prettier from 'eslint-plugin-prettier';
-import prettierConfig from 'eslint-config-prettier';
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+import prettierConfig from "eslint-config-prettier";
 
+/**
+ * @type {import('eslint').Linter.Config[]}
+ */
 export default [
   {
-    ignores: ['build/**', 'examples/build/**']
+    ignores: ["build/**"],
   },
   prettierConfig,
   {
-    files: ['**/*.ts'],
+    files: ["**/*.{js,ts}"],
     languageOptions: {
-      parser: tsparser,
+      parser: tsParser,
       parserOptions: {
-        project: './tsconfig.json',
-        ecmaVersion: 2018,
-        sourceType: 'module'
+        project: "./tsconfig.json",
+        sourceType: "module",
       },
       globals: {
         window: false,
-        document: false
-      }
+        document: false,
+      },
     },
     plugins: {
-      '@typescript-eslint': tseslint,
-      'prettier': prettier
+      "@typescript-eslint": tseslint,
+      prettier: prettier,
     },
     rules: {
       ...tseslint.configs.recommended.rules,
-      'prettier/prettier': 'warn',
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-use-before-define': 'off',
-      '@typescript-eslint/prefer-for-of': 'error',
-      '@typescript-eslint/no-for-in-array': 'error',
-      '@typescript-eslint/no-require-imports': 'error',
-      '@typescript-eslint/no-parameter-properties': 'off',
-      '@typescript-eslint/explicit-member-accessibility': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', {
-        'vars': 'all',
-        'args': 'after-used',
-        'ignoreRestSiblings': true
-      }],
-      'no-shadow': 'off',
-      '@typescript-eslint/no-shadow': 'error',
-      '@typescript-eslint/consistent-type-assertions': ['error', {
-        'assertionStyle': 'as',
-        'objectLiteralTypeAssertions': 'allow-as-parameter'
-      }],
-      '@typescript-eslint/no-namespace': 'error',
-      'linebreak-style': ['error', 'unix'],
-      'no-irregular-whitespace': ['error', {
-        'skipComments': true
-      }],
-      'no-alert': 'error',
-      'prefer-const': 'error',
-      'no-return-assign': 'error',
-      'no-useless-call': 'error',
-      'no-useless-concat': 'error',
-      'no-console': 'off',
-      'no-undef': 'off',
-      'no-unreachable': 'off'
-    }
-  }
+      "prettier/prettier": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-use-before-define": "off",
+      "@typescript-eslint/prefer-for-of": "error",
+      "@typescript-eslint/no-for-in-array": "error",
+      "@typescript-eslint/no-require-imports": "error",
+      "@typescript-eslint/no-parameter-properties": "off",
+      "@typescript-eslint/explicit-member-accessibility": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: true,
+        },
+      ],
+      "no-shadow": "off",
+      "@typescript-eslint/no-shadow": "error",
+      "@typescript-eslint/consistent-type-assertions": [
+        "error",
+        {
+          assertionStyle: "as",
+          objectLiteralTypeAssertions: "allow-as-parameter",
+        },
+      ],
+      "@typescript-eslint/no-namespace": "error",
+      "linebreak-style": ["error", "unix"],
+      "no-irregular-whitespace": [
+        "error",
+        {
+          skipComments: true,
+        },
+      ],
+      "no-alert": "error",
+      "prefer-const": "error",
+      "no-return-assign": "error",
+      "no-useless-call": "error",
+      "no-useless-concat": "error",
+      "no-console": "off",
+      "no-undef": "off",
+      "no-unreachable": "off",
+    },
+  },
 ];
